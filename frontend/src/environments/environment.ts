9f155fda6cc0a9a29949762c1ea57efe7fd71ad7
kt_ismail <ktismail97@gmail.com>
2025-09-15 00:51:24 +0400

    bill updated

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   // apiUrl: 'http://172.20.10.2:3000',
   appName: 'Tech Lube',
   contact_number: '+971 55 860 7006',
-  email:'info@techlubeuae.com',
+  email: 'techlubeuae@gmail.com',
   website: 'www.techlubeuae.com'
 };
